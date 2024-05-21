34e977efb74e132a61e45dd506388ea074724135
Brandon <brandon.ohboil@gmail.com>
2024-05-21 02:10:35 -0400

    fixed slider hopefully

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 
-function SlideOutCard({ tabContent, children}) {
+interface SlideOutCardProps {
+  tabContent: React.ReactNode; // Explicitly define tabContent type
+  children: React.ReactNode;
+}
+
+function SlideOutCard({ tabContent, children }: SlideOutCardProps) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleCard = () => {
@@ -8,12 +13,17 @@ function SlideOutCard({ tabContent, children}) {
   };
 
   return (
-    <div className="relative"> {/* Container */}
-      <div className="fixed right-0 top-1/2 -translate-y-1/2 bg-gray-200 p-2 px-4 rounded-l-md cursor-pointer" onClick={toggleCard}>
-        {tabContent} 
+    <div className="relative"> 
+      <div 
+        className="fixed right-0 top-1/2 -translate-y-1/2 bg-gray-200 p-2 px-4 rounded-l-md cursor-pointer z-10"
+        onClick={toggleCard}
+      >
+        {tabContent}
       </div>
-      <div className={`fixed right-[-100%] top-0 h-screen w-80 bg-white shadow-lg transition-right duration-300 ease-out ${isExpanded ? 'right-0' : ''}`}>
-        {children} 
+      <div 
+        className={`fixed right-full top-0 h-screen w-80 bg-white shadow-lg transition-transform duration-300 ease-out ${isExpanded ? 'translate-x-full' : ''}`}
+      >
+        {children}
       </div>
     </div>
   );
